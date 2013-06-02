e74197a06ebde5d574763dba2af8de6ca1ce83ae
Anuj Dutta <anuj@andhapp.com>
2013-06-02 18:53:26 +0100

    Add code to initialise rspec-fire.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ require "bundler"
 Bundler.require
 
 require 'ostruct'
+require 'rspec/fire'
 require File.expand_path("../../lib/textrazor" ,__FILE__)
+
+RSpec.configure do |config|
+  config.include(RSpec::Fire)
+end
