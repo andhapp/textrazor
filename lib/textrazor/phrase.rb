805d0bcf4adee2a6e65a96116c357ac6c5617dbe
Michael He <hlxwell@gmail.com>
2018-02-05 13:55:23 +0900

    remove unnecessary create_from_hash method

--- lib/textrazor/phrase.rb
+++ lib/textrazor/phrase.rb
@@ -1,18 +1,11 @@
 module TextRazor
 
   class Phrase
-    include TextRazor::Util
-
     attr_reader :id, :text
 
     def initialize(params, words)
-      params = initialize_params params
       @id    = params["id"]
       @text  = match_words(params["wordPositions"], words)
-    end
-
-    def self.create_from_hash(params, words)
-      new(params, words)
     end
 
     def match_words(positions, words)
